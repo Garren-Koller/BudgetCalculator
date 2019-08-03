353fb5e566760fcd7e8c35951f32aacd8cfcb8d2
Ke'Ondrae <keomell@uat.edu>
2019-08-03 16:59:24 -0400

    Updated Expence

--- BudgetCalculator/Expenses.h
+++ BudgetCalculator/Expenses.h
@@ -3,6 +3,68 @@
 class Expenses : public Calculator
 {
 
+public:
+	Expenses();
+	~Expenses();
+
+private:
+	// Housing
+	double mortgage;
+	double mortgage2nd;
+	double insurance;
+	double propertyTax;
+	double homeMaintenance;		
+	double electricGas;
+	double waterSewer;
+	double phoneCell;
+	double cableInternet;
+
+	//Transportation
+	double carPayment;
+	double carPayment2nd;
+	double monthlyCarInsurance;
+	double gasoline;
+	double carMaintenance;
+	double parkingTolls;
+
+	//Food
+	double groceriesHouseholdGoods;
+	double schoolLunches;
+	double mealsOut;
+
+	//Health 
+	double healthInsurance;
+	double prescriptions;
+	double lifeInsurance;
+	double clothingForFamily;
+	double dryCleaningLaundry;
+	double barberBeautyShop;
+
+	//Children
+	double childCare;
+	double childSupportPayments;
+	double schoolTuitionSupplies;
+
+	//Debt and loans
+	double creditCards;
+	double storeCards;
+	double medicalDebt;
+	double studentLoans;
+	double taxPayments;
+	double miscellaneousDebtPayments;
+
+	//Misc. 
+	double hobbiesSports;
+	double tobaccoAlcohol;
+	double newspaperMagazines;
+	double travelRecreation;
+	double churchCharitableDonations;
+	double petCare;
+	double holidaysBirthdays;
+	double otherExpenses;
+
+protected:
+
 
 };
 
