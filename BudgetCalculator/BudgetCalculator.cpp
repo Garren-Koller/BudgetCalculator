d863bc1f76786b1c77617504ec9e995fb7e1f4d2
GARREN KOLLER <GARKOLLE@uat.edu>
2019-08-03 12:18:51 -0700

    s

--- BudgetCalculator/BudgetCalculator.cpp
+++ BudgetCalculator/BudgetCalculator.cpp
@@ -4,11 +4,64 @@
 #include "pch.h"
 #include <iostream>
 
+using std::cout;
+using std::endl;
+
+void Intro();
+
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	Intro();
 }
 
 
+<<<<<<< Updated upstream
 
 
+=======
+void Intro()
+{
+	/////////////////////////////////////////////////////////////////// Intro
+
+	cout << "-----Budget Calculator-----" << endl;
+
+	cout << "This is a budget calculator" << endl;
+	cout << "" << endl;
+	cout << "It is used to calculate your income and expenses monthly." << endl;
+	cout << "Please use all monthly costs. " << endl;
+
+	//////////////////////////////////////////////////////////////////
+	cout << "" << endl;
+	cout << "" << endl;
+	//////////////////////////////////////////////////////////////////	First Step
+
+	cout << "-----Budget Calculator-----" << endl;
+	cout << "" << endl;
+	cout << "-Step 1" << endl;
+	cout << "1. Net Monthly Pay" << endl;
+	cout << "2. Other Monthly Income" << endl;
+	cout << "3. Total Monthly Income" << endl;
+
+	//////////////////////////////////////////////////////////////////
+	cout << "" << endl;
+	//////////////////////////////////////////////////////////////////	Second Step
+
+	cout << "-Step 2" << endl;
+	cout << "1. Housing & Utilities" << endl;
+	cout << "2. Transportation" << endl;
+	cout << "3. Food & Groceries" << endl;
+	cout << "4. Health & Beauty" << endl;
+	cout << "5. Children" << endl;
+	cout << "6. Debt & Loan Payments" << endl;
+	cout << "7. Misc. Expenses" << endl;
+	cout << "8. Total Monthly Expenses" << endl;
+
+	//////////////////////////////////////////////////////////////////
+	cout << "" << endl;
+	//////////////////////////////////////////////////////////////////
+
+	cout << "Please select an object by entering a number to start calculating." << endl;
+
+	system("pause");
+}
+>>>>>>> Stashed changes
