bd309a88c55207df6e45daf253b641362badae30
GARREN KOLLER <GARKOLLE@uat.edu>
2019-08-03 17:28:36 -0700

    Expence

--- Expenses.cpp
+++ Expenses.cpp
@@ -15,3 +15,163 @@ void Expenses::setMortgage(double mortgage) {
 double Expenses::getMortgage() {
     return 0;
 }
+
+void Expenses::setMortgage2nd(double mortgage2nd) {
+
+}
+
+void Expenses::setInsurance(double insurance) {
+
+}
+
+void Expenses::setPropertyTax(double propertyTax) {
+
+}
+
+void Expenses::setHomeMaintenance(double homeMaintenance) {
+
+}
+
+void Expenses::setElectricGas(double electricGas) {
+
+}
+
+void Expenses::setWaterSewer(double waterSewer) {
+
+}
+
+void Expenses::setPhoneCell(double phoneCell) {
+
+}
+
+void Expenses::setCableInternet(double cableInternet) {
+
+}
+
+void Expenses::setCarPayment(double carPayment) {
+
+}
+
+void Expenses::setCarPayment2nd(double carPayment2nd) {
+
+}
+
+void Expenses::setMonthlyCarInsurance(double monthlyCarInsurance) {
+
+}
+
+void Expenses::setGasoline(double gasoline) {
+
+}
+
+void Expenses::setCarMaintenance(double carMaintenance) {
+
+}
+
+void Expenses::setParkingTolls(double parkingTolls) {
+
+}
+
+void Expenses::setGroceriesHouseholdGoods(double groceriesHouseholdGoods) {
+
+}
+
+void Expenses::setSchoolLunches(double schoolLunches) {
+
+}
+
+void Expenses::setMealsOut(double mealsOut) {
+
+}
+
+void Expenses::setHealthInsurance(double healthInsurance) {
+
+}
+
+void Expenses::setPrescriptions(double prescriptions) {
+
+}
+
+void Expenses::setLifeInsurance(double lifeInsurance) {
+
+}
+
+void Expenses::setClothingForFamily(double clothingForFamily) {
+
+}
+
+void Expenses::setDryCleaningLaundry(double dryCleaningLaundry) {
+
+}
+
+void Expenses::setBarberBeautyShop(double barberBeautyShop) {
+
+}
+
+void Expenses::setChildCare(double childCare) {
+
+}
+
+void Expenses::getChildSupportPayments(double childSupportPayments) {
+
+}
+
+void Expenses::getSchoolTuitionSupplies(double schoolTuitionSupplies) {
+
+}
+
+void Expenses::getCreditCards(double creditCards) {
+
+}
+
+void Expenses::getStoreCards(double storeCards) {
+
+}
+
+void Expenses::getMedicalDebt(double medicalDebt) {
+
+}
+
+void Expenses::getStudentLoans(double studentLoans) {
+
+}
+
+void Expenses::getTaxPayments(double taxPayments) {
+
+}
+
+void Expenses::getMiscellaneousDebtPayments(double miscellaneousDebtPayments) {
+
+}
+
+void Expenses::getHobbiesSports(double hobbiesSports) {
+
+}
+
+void Expenses::getTobaccoAlcohol(double tobaccoAlcohol) {
+
+}
+
+void Expenses::getNewspaperMagazines(double newspaperMagazines) {
+
+}
+
+void Expenses::getTravelRecreation(double travelRecreation) {
+
+}
+
+void Expenses::getChurchCharitableDonations(double churchCharitableDonations) {
+
+}
+
+void Expenses::getPetCare(double petCare) {
+
+}
+
+void Expenses::getHolidaysBirthdays(double holidaysBirthdays) {
+
+}
+
+void Expenses::getOtherExpenses(double otherExpenses) {
+
+}
--- Expenses.h
+++ Expenses.h
@@ -7,8 +7,117 @@ public:
 	Expenses();
 	~Expenses();
 
+    // Housing
 	void setMortgage(double mortgage);
+    void setMortgage2nd(double mortgage2nd);
+    void setInsurance(double insurance);
+    void setPropertyTax(double propertyTax);
+    void setHomeMaintenance(double homeMaintenance);
+    void setElectricGas(double electricGas);
+    void setWaterSewer(double waterSewer);
+    void setPhoneCell(double phoneCell);
+    void setCableInternet(double cableInternet);
+
+    //Transportation
+    void setCarPayment(double carPayment);
+    void setCarPayment2nd(double carPayment2nd);
+    void setMonthlyCarInsurance(double monthlyCarInsurance);
+    void setGasoline(double gasoline);
+    void setCarMaintenance(double carMaintenance);
+    void setParkingTolls(double parkingTolls);
+
+    //Food
+    void setGroceriesHouseholdGoods(double groceriesHouseholdGoods);
+    void setSchoolLunches(double schoolLunches);
+    void setMealsOut(double mealsOut);
+
+    //Health
+    void setHealthInsurance(double healthInsurance);
+    void setPrescriptions(double prescriptions);
+    void setLifeInsurance(double lifeInsurance);
+    void setClothingForFamily(double clothingForFamily);
+    void setDryCleaningLaundry(double dryCleaningLaundry);
+    void setBarberBeautyShop(double barberBeautyShop);
+
+    //Children
+    void setChildCare(double childCare);
+    void getChildSupportPayments(double childSupportPayments);
+    void getSchoolTuitionSupplies(double schoolTuitionSupplies);
+
+    //Debt and loans
+    void getCreditCards(double creditCards);
+    void getStoreCards(double storeCards);
+    void getMedicalDebt(double medicalDebt);
+    void getStudentLoans(double studentLoans);
+    void getTaxPayments(double taxPayments);
+    void getMiscellaneousDebtPayments(double miscellaneousDebtPayments);
+
+    //Misc.
+    void getHobbiesSports(double hobbiesSports);
+    void getTobaccoAlcohol(double tobaccoAlcohol);
+    void getNewspaperMagazines(double newspaperMagazines);
+    void getTravelRecreation(double travelRecreation);
+    void getChurchCharitableDonations(double churchCharitableDonations);
+    void getPetCare(double petCare);
+    void getHolidaysBirthdays(double holidaysBirthdays);
+    void getOtherExpenses(double otherExpenses);
+
+////////////////////////////////////////////////////////////////////////////////
+    // Housing
 	double getMortgage();
+    double getMortgage2nd();
+    double getInsurance();
+    double getPropertyTax();
+    double getHomeMaintenance();
+    double getElectricGas();
+    double getWaterSewer();
+    double getPhoneCell();
+    double getCableInternet();
+
+    //Transportation
+    double getCarPayment();
+    double getCarPayment2nd();
+    double getMonthlyCarInsurance();
+    double getGasoline();
+    double getCarMaintenance();
+    double getParkingTolls();
+
+    //Food
+    double getGroceriesHouseholdGoods();
+    double getSchoolLunches();
+    double getMealsOut();
+
+    //Health
+    double getHealthInsurance();
+    double getPrescriptions();
+    double getLifeInsurance();
+    double getClothingForFamily();
+    double getDryCleaningLaundry();
+    double getBarberBeautyShop();
+
+    //Children
+    double getChildCare();
+    double getChildSupportPayments();
+    double getSchoolTuitionSupplies();
+
+    //Debt and loans
+    double getCreditCards();
+    double getStoreCards();
+    double getMedicalDebt();
+    double getStudentLoans();
+    double getTaxPayments();
+    double getMiscellaneousDebtPayments();
+
+    //Misc.
+    double getHobbiesSports();
+    double getTobaccoAlcohol();
+    double getNewspaperMagazines();
+    double getTravelRecreation();
+    double getChurchCharitableDonations();
+    double getPetCare();
+    double getHolidaysBirthdays();
+    double getOtherExpenses();
+
 
 private:
 	// Housing
